2bddaeecbfbe1a928df71dbbc2df630ae19bdca2
javo26 <javier.marroqui26@gmail.com>
2023-03-10 11:11:02 -0600

    segundos cambios

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, ipcMain} = require('electron');
+const path = require('path')
+
+let ventana;
 
 function createWindow(){
     const ventana = new BrowserWindow({
         width: 500,
-        height: 300
+        height: 300,
+        webPreferences:{
+            preload: path.join(app.getAppPath(),'preload.js')
+        }
     })
     ventana.loadFile('index.html')
 }
+
+ipcMain.on('registroValido', function(event,args){
+    console.log(args)
+})
+    
+
 
 app.whenReady().then(createWindow)
