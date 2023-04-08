8692ea4cb849030085417034ded2da632fcf4b0c
javo26 <javier.marroqui26@gmail.com>
2023-04-07 20:39:11 -0600

    Aplicación sin usuario en pantalla 2

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 var registro = document.getElementById('registro')
-//var nombre = document.getElementById('name')
-//var correo = document.getElementById('mail')
-//var pass = document.getElementById('pass')
-//var fechaNacimiento = document.getElementById('birth')
+var nombre = document.getElementById('name')
+var correo = document.getElementById('email')
+var pass = document.getElementById('pass')
+var confirmPass = document.getElementById('confirmPass')
+var fechaN = document.getElementById('birth')
 
-//var expEmail = RegExp('^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+var expMay = RegExp('[A-Z]')
+var expMin = RegExp('[a-z]')
+var expNum = RegExp('[0-9]')
+var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+var expPass = RegExp("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-aZ-Z0-9]).{8,}")
+
+function calculoEdad(fecha_n){
+   var fecha = new Date(fecha_n);
+   var anio = fecha.getFullYear();
+   var fechaActual = new Date();
+   var anioActual = fechaActual.getFullYear();
+   return anioActual - anio
+}
+
 
 registro.addEventListener('click',function(evento){
     evento.preventDefault()
-    alert('Bienvenido ' + document.getElementById('name').value)
-   
+
+    var error=''
+    var edad = calculoEdad(fechaN.value) 
+    
+    if (nombre.value.length < 6 ){
+        error += 'Pruebe un nombre de usuario mas largo.\n'
+    }
+    if (nombre.value.match(expMay)){
+        error += 'El nombre de usuario no debe de llevar Mayusculas.\n'
+    }
+    if (!correo.value.match(validEmail)){
+        error +='Correo no valido.\n'
+    }
+    if (!pass.value.match(expPass)){
+        error += 'El campo de password esta vacio o no tiene el formato correcto.\n'
+    }
+    if(!pass.value.match(confirmPass.value)){
+        error += 'Las contraseñas no coinciden.\n'
+    }
+    if (edad < 18){
+        error += 'El registro es para mayores de edad.\n'
+    }
+    if (error == ''){
+        window.comunicacion.registroValido([nombre.value]);
+    }else{
+        alert(error)
+    }   
+    
 })
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const {app, BrowserWindow, ipcMain} = require('electron');
 const path = require('path')
 
 let ventana;
+let ventana2;
 
 function createWindow(){
     ventana = new BrowserWindow({
@@ -13,9 +14,24 @@ function createWindow(){
     })
     ventana.loadFile('index.html')
 }
+function createWindow2(){
+    ventana2 = new BrowserWindow ({
+        width: 500,
+        height: 300,
+        webPreferences:{
+            preload: path.join(app.getAppPath(),'preload.js')
+        }
+    });
+    ventana2.loadFile('pantalla2.html')
+}
 
 ipcMain.on('registroValido', function(event,args){
     console.log(args)
+    let usuario = args
+    createWindow2()
+    ventana2.webContents.on('did-finish-load',function(){
+        ventana2.webContents.send('inicioCorrecto','Bienvenido')
+    })
 })
     
 
